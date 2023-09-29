dcdc9d5c2f24d19eddfbfa69aac81fa8ce458f15
shivanitiwari19 <shivani.2125it1065@kiet.edu>
2023-09-30 00:42:43 +0530

    Added new file

---
+++ src/Conditionals/Salary.java
@@ -0,0 +1,18 @@
+package Conditionals;
+
+import java.util.Scanner;
+
+public class Salary {
+    public static void main(String[] args) {
+        System.out.print("Enter salary: ");
+        Scanner input = new Scanner(System.in);
+        int sal = input.nextInt();
+        if (sal >10000) {
+            sal = sal + 2000;
+        }
+        else {
+            sal = sal + 1000;
+        }
+        System.out.println("Effective Salary is : "+ sal);
+    }
+}

6bf58d1df320b1562bf89c7a730704c25ac7244c
shivanitiwari19 <shivani.2125it1065@kiet.edu>
2023-09-29 22:36:56 +0530

    Added new file

---
+++ src/com/First/Inputs.java
@@ -0,0 +1,19 @@
+package com.First;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Inputs {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+//        System.out.print("Enter your roll no.");
+//        int roll.no = input.nextInt();
+//        System.out.println("Your roll no. is " + rollno);
+
+        int a = 234_000_000;  //underscores are ignored.
+        // 10 is a literal. 'a' is called the identifier.
+
+        float marks = input.nextFloat();
+        System.out.println(marks);
+    }
+}

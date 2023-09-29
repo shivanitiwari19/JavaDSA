62594bab0940b66e139a1d7b79a3e862dd9c9b5b
shivanitiwari19 <shivani.2125it1065@kiet.edu>
2023-09-29 22:40:06 +0530

    Added new file

---
+++ src/com/First/Main.java
@@ -0,0 +1,12 @@
+package com.First;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("Hello World! ");
+        System.out.println("My name is Shivani.");
+        Scanner input = new Scanner(System.in);
+        System.out.println(input.nextLine());
+    }
+}

d839163a2d5bc81be811cbd8002e581badaab13c
shivanitiwari19 <shivani.2125it1065@kiet.edu>
2023-09-29 22:42:01 +0530

    Added new file

---
+++ src/com/First/Primitives.java
@@ -0,0 +1,21 @@
+package com.First;
+//Primitive data types are the ones that cannot be broken further.
+public class Primitives {
+    public static void main(String[] args){
+        int roll_no = 161;
+        char letter = 'r';
+        //Single quotes for char, double for strings.
+
+        float marks = 98.5f;
+        double position = 54325456.789746;
+        //double is used for large decimal numbers.
+
+        long marks2 = 7548395456437564L;
+        //long is used for large integer numbers.
+
+        boolean check = false;
+        //String is not a primitive datatype. Primitive means that cannot be broken even further.
+
+
+    }
+}

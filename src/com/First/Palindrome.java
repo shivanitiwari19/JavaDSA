41afc0c62bdca72874f2cf023814f4df0aea82df
shivanitiwari19 <shivani.2125it1065@kiet.edu>
2023-09-29 22:40:52 +0530

    Added new file

---
+++ src/com/First/Palindrome.java
@@ -0,0 +1,10 @@
+package com.First;
+
+import java.util.Scanner;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String str = input.next();
+    }
+}

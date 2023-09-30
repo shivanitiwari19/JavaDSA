1dba7a6e588716c5fbc98cb031c8bf078d1c0d68
shivanitiwari19 <shivani.2125it1065@kiet.edu>
2023-09-30 13:23:38 +0530

    New file added

---
+++ src/Conditionals/Calculator.java
@@ -0,0 +1,40 @@
+package Conditionals;
+
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int ans = 0;
+
+        while (true) {
+            System.out.print("Enter the operator: ");
+            char op = in.next().trim().charAt(0);
+            if (op == '+' || op == '-' || op == '*' || op == '/' || op == '%') {
+                System.out.print("Enter two numbers: ");
+                int a = in.nextInt();
+                int b = in.nextInt();
+
+                if (op == '+')
+                    ans = a + b;
+                if (op == '-')
+                    ans = a - b;
+                if (op == '*')
+                    ans = a * b;
+                if (op == '/') {
+                    if (b == 0)
+                        System.out.println("Not possible.");
+                    else
+                        ans = a / b;
+                }
+                if (op == '%')
+                    ans = a % b;
+            }
+            else if (op == 'x' || op == 'X')
+                break;
+            else
+                System.out.println("Invalid operator!");
+            System.out.println("Answer: "+ans);
+        }
+    }
+}

5e50151408ee9dac544c310d3105bcd179f15c8c
shivanitiwari19 <shivani.2125it1065@kiet.edu>
2023-10-02 12:55:36 +0530

    File updated.

--- src/Functions/Greeting.java
+++ src/Functions/Greeting.java
@@ -1,8 +1,21 @@
 package Functions;
 
+import java.util.Scanner;
+
 public class Greeting {
     public static void main(String[] args) {
-        greeting();
+//        greeting();
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = in.next();
+        String personalized = myGreet(name);
+        System.out.println(personalized);
+    }
+
+    static String myGreet(String name) {
+        String message = "Hello " + name;
+        return message;
     }
 
     static void greeting()                // this function doesn't return anythihg.
--- src/Functions/Sum.java
+++ src/Functions/Sum.java
@@ -4,11 +4,18 @@ import java.util.Scanner;
 
 public class Sum {
     public static void main(String[] args) {
-        sum();     //function call
-        int ans = sum2();                   // the return value gets stored into the reference variable ans.
+//        sum();     //function call
+//        int ans = sum2();                   // the return value gets stored into the reference variable ans.
+//        System.out.println(ans);
+        int ans = sum3(3,5);
         System.out.println(ans);
     }
 
+    // pass the value of numbers when you are calling the method in main()
+    static int sum3(int a, int b){
+        int sum = a+b;
+        return sum;
+    }
     static int sum2() {
         Scanner in = new Scanner(System.in);
         System.out.print("Enter number 1: ");

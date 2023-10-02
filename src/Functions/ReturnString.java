5a08b1ea691aa4f362a60cd9cf08a212a12a20dc
shivanitiwari19 <shivani.2125it1065@kiet.edu>
2023-10-02 12:43:29 +0530

    New file added

---
+++ src/Functions/ReturnString.java
@@ -0,0 +1,13 @@
+package Functions;
+
+public class ReturnString {
+    public static void main(String[] args) {
+        String message = greet();
+        System.out.println(message);
+    }
+
+    static String greet() {
+        String greeting = "How are you?";
+        return greeting;
+    }
+}

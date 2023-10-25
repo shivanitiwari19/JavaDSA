5c2a99697af5f584fbbabad35dd61157913075c0
shivanitiwari19 <shivani.2125it1065@kiet.edu>
2023-10-25 22:44:00 +0530

    File updated.

--- src/Arrays/ObjArray.java
+++ src/Arrays/ObjArray.java
@@ -1,4 +1,18 @@
 package Arrays;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class ObjArray {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String[] str = new String[4];
+        for (int i = 0 ; i < str.length ; i++){
+            str[i] = in.next();                    // it will take character as well as string input.
+        }
+        System.out.println(Arrays.toString(str));
+
+        str[3] = "Saakshi";
+        System.out.println(Arrays.toString(str));
+    }
 }

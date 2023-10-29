d17d7636c86e7f054a0ea96f889b245caffdf066
shivanitiwari19 <shivani.2125it1065@kiet.edu>
2023-10-29 13:55:21 +0530

    File updated.

--- src/LinearSearch/EvenDigits.java
+++ src/LinearSearch/EvenDigits.java
@@ -4,6 +4,7 @@ public class EvenDigits {
     public static void main(String[] args) {
         int[] nums = {12,45,3443,785,-99};
         System.out.println(findnumbers(nums));
+        System.out.println(digits2(34566764));
     }
 
     static int findnumbers(int[] nums){
@@ -19,6 +20,9 @@ public class EvenDigits {
         return numberofdidgits % 2 == 0;
     }
 
+    static int digits2(int num){
+        return (int)(Math.log10(num)) + 1;
+    }
     static int digits(int num){
         if(num<0)
             num=num*-1;

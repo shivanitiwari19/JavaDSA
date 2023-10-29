f5473cff3bf759fe289414fe242deab5dc0d7380
shivanitiwari19 <shivani.2125it1065@kiet.edu>
2023-10-29 13:47:31 +0530

    File added.

---
+++ src/LinearSearch/EvenDigits.java
@@ -0,0 +1,37 @@
+package LinearSearch;
+
+public class EvenDigits {
+    public static void main(String[] args) {
+        int[] nums = {12,45,3443,785,-99};
+        System.out.println(findnumbers(nums));
+    }
+
+    static int findnumbers(int[] nums){
+        int count=0;
+        for(int num : nums){
+            if (even(num))
+                count++;
+            }
+        return count;
+    }
+    static boolean even(int num){
+        int numberofdidgits = digits(num);
+        return numberofdidgits % 2 == 0;
+    }
+
+    static int digits(int num){
+        if(num<0)
+            num=num*-1;
+
+        if(num==0)
+            return 1;
+
+        int count = 0;
+
+        while (num>0) {
+            count++;
+            num=num/10;
+        }
+        return count;
+    }
+}

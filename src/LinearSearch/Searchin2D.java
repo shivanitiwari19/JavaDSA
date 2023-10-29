315a6f4d9e9f1c611d25923dfd797b2873fe6464
shivanitiwari19 <shivani.2125it1065@kiet.edu>
2023-10-29 13:32:11 +0530

    File added.

--- src/LinearSearch/Searchin2D.java
+++ src/LinearSearch/Searchin2D.java
@@ -10,9 +10,11 @@ public class Searchin2D {
                 {2, 4, 98},
                 {67, 44}
         };
-        int target = 55;
+        int target = 44;
         int[] ans = search(arr, target);
         System.out.println(Arrays.toString(ans));
+        int max=searchMax(arr);
+        System.out.println(max);
     }
 
     static int[] search(int[][] arr, int target){
@@ -21,8 +23,18 @@ public class Searchin2D {
                 if(arr[i][j]==target)
                     return new int[]{i, j};
             }
-
         }
         return new int[] {-1,-1};
+    }
+
+    static int searchMax(int[][] arr){
+        int max = arr[0][0];
+        for(int i=0 ; i<arr.length ; i++){
+            for(int j=0; j<arr[i].length ; j++){
+                if(arr[i][j]>max)
+                    max=arr[i][j];
+            }
+        }
+        return max;
     }
 }

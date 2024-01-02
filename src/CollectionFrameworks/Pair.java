408d1ba6ad4769e96079e4b41e9075e1efa404c9
shivanitiwari19 <shivani.2125it1065@kiet.edu>
2024-01-02 20:58:50 +0530

    File updated.

---
+++ src/CollectionFrameworks/Pair.java
@@ -0,0 +1,14 @@
+package CollectionFrameworks;
+
+public class Pair<X,Y> {
+    X x;
+    Y y;
+    public Pair(X x, Y y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public void getDesc(){
+        System.out.println(x + " and " + y);
+    }
+}

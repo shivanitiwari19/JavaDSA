4d42bf5c1f88ebd36ccd234f911739dcc2052ce2
shivanitiwari19 <shivani.2125it1065@kiet.edu>
2024-01-03 10:13:21 +0530

    File updated.

--- src/CollectionFrameworks/MyArrayList.java
+++ src/CollectionFrameworks/MyArrayList.java
@@ -2,8 +2,8 @@ package CollectionFrameworks;
 
 public class MyArrayList {
     public static void main(String[] args) {
-        Pair<Integer, String> p1 = new Pair (11 , "Shivani");
-        Pair<Boolean, String> p2 = new Pair (true, "Hey, there!");
+        Pair<Integer, String> p1 = new Pair<>(11 , "Shivani");
+        Pair<Boolean, String> p2 = new Pair<> (true, "Hey, there!");
         p1.getDesc();
         p2.getDesc();
     }

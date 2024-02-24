26347601fcb6f68fdb74c385d0dfaf2370130af1
shivanitiwari19 <shivani.2125it1065@kiet.edu>
2024-02-25 02:00:16 +0530

    File added.

---
+++ src/Strings/Comparison.java
@@ -0,0 +1,11 @@
+package Strings;
+
+public class Comparison {
+    public static void main(String[] args) {
+        String a = "Shivani";
+        String b = "Shivani";
+
+        // ==
+        System.out.println(a.equals(b));   // gives true
+    }
+}

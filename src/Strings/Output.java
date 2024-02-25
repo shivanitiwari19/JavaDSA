5f009c679722d630f9d1f3918327be02caf0c4fe
shivanitiwari19 <shivani.2125it1065@kiet.edu>
2024-02-25 16:13:08 +0530

    File updated.

--- src/Strings/Output.java
+++ src/Strings/Output.java
@@ -2,11 +2,11 @@ package Strings;
 import java.util.Arrays;
 public class Output {
     public static void main(String[] args) {
-//        System.out.println(56);
-//        System.out.println("Richa");
+        System.out.println(56);
+        System.out.println("Richa");
         System.out.println(Arrays.toString(new int[]{2,3,4,5}));    //
 //
-//        String name = null;
-//        System.out.println(name);
+        String name = null;
+        System.out.println(name);
     }
 }

a5ddf157821a3baa9bcf49813d5300c7acb34640
shivanitiwari19 <shivani.2125it1065@kiet.edu>
2024-02-25 15:39:30 +0530

    File updated.

--- src/Strings/Comparison.java
+++ src/Strings/Comparison.java
@@ -6,6 +6,13 @@ public class Comparison {
         String b = "Shivani";
 
         // ==
-        System.out.println(a.equals(b));   // gives true
+        System.out.println(a == b);   // gives true because both the ref vars are pointing to the same object because
+                                           // duplicate values are not allowed in the string pool.
+
+        String name1 = new String("Shivani");
+        String name2 = new String("Shivani");
+
+        System.out.println(name1 == name2);   // this will give false because the objects are created outside the string pool
+                                              // and the ref vars are pointing to two different objects with the same value
     }
 }

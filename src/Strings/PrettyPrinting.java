8636252d0aaae9b195b52077fd408195d2ac3250
shivanitiwari19 <shivani.2125it1065@kiet.edu>
2024-02-25 16:28:54 +0530

    File updated.

---
+++ src/Strings/PrettyPrinting.java
@@ -0,0 +1,15 @@
+package Strings;
+
+public class PrettyPrinting {
+    public static void main(String[] args) {
+        float a = 453.4586f;
+        System.out.printf("Formatted number is %.2f", a);
+        System.out.println("\n");
+        System.out.println(Math.PI);
+        System.out.printf("Pi : %.3f", Math.PI);
+
+        System.out.println("\n");
+        System.out.printf("My name is %s and I %s.", "Shivani", "sing");
+
+    }
+}

2c7a835a6107419b6d2a713f6a0da5fd3ed41e72
shivanitiwari19 <shivani.2125it1065@kiet.edu>
2024-02-25 16:12:30 +0530

    File added.

---
+++ src/Strings/Output.java
@@ -0,0 +1,12 @@
+package Strings;
+import java.util.Arrays;
+public class Output {
+    public static void main(String[] args) {
+//        System.out.println(56);
+//        System.out.println("Richa");
+        System.out.println(Arrays.toString(new int[]{2,3,4,5}));    //
+//
+//        String name = null;
+//        System.out.println(name);
+    }
+}

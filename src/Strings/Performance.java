f49c483608abcaef4979211fe6c597fd7b888853
shivanitiwari19 <shivani.2125it1065@kiet.edu>
2024-02-27 10:41:41 +0530

    File added.

--- src/Strings/Performance.java
+++ src/Strings/Performance.java
@@ -2,6 +2,10 @@ package Strings;
 
 public class Performance {
     public static void main(String[] args) {
-
+        String series = "";
+        for(int i =0 ; i<26 ; i++){
+            char ch = (char) ('a' + i);
+            System.out.println(ch);
+        }
     }
 }

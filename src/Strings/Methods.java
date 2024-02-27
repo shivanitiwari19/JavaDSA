9624246d94e0f606a478fa1635c8ef54f70543cd
shivanitiwari19 <shivani.2125it1065@kiet.edu>
2024-02-27 11:38:42 +0530

    File updated.

--- src/Strings/Methods.java
+++ src/Strings/Methods.java
@@ -2,11 +2,12 @@ package Strings;
 import java.util.Arrays;
 public class Methods {
     public static void main(String[] args) {
-        String name = "Shivani";
+        String name = "Shivani Tiwari Hello World";
         System.out.println(Arrays.toString(name.toCharArray()));
         System.out.println(name.toLowerCase());
         System.out.println(name);
         System.out.println(name.indexOf('a'));
         System.out.println("   Shivani   ".strip());
+        System.out.println(Arrays.toString(name.split(" ")));
     }
 }

75c4e769667bd3f72f6219e1179a91061ab967d2
shivanitiwari19 <shivani.2125it1065@kiet.edu>
2024-02-29 10:49:08 +0530

    File added.

--- src/Temp.java
+++ src/Temp.java
@@ -1,7 +1,29 @@
 public class Temp {
+    public static int[][] generateCombinations(int num1, int num2) {
+        int[][] combinations = new int[num1][num2];
+
+        for (int i = 0; i < num1; i++) {
+            for (int j = 0; j < num2; j++) {
+                combinations[i][j] = (i + 1) * (j + 1);
+            }
+        }
+
+        return combinations;
+    }
+
+    public static void printMatrix(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
-        int m = 5;
-        int n = 12;
-        System.out.println(8&7);
+        int num1 = 5;
+        int num2 = 3;
+        int[][] resultMatrix = generateCombinations(num1, num2);
+        printMatrix(resultMatrix);
     }
 }

f20f1213ac142a9d4c1e5cc2937f1980e4df9c8c
shivanitiwari19 <shivani.2125it1065@kiet.edu>
2024-02-29 10:49:43 +0530

    File updated.

--- src/Recursion/NumberExNew.java
+++ src/Recursion/NumberExNew.java
@@ -2,15 +2,19 @@ package Recursion;
 
 public class NumberExNew {
     public static void main(String[] args) {
+        // first function call
         print(1);
     }
 
     static void print(int n){
+        // base condition
         if(n == 5){
             System.out.println(5);
             return;
         }
         System.out.println(n);
+
+        // Recursive function call
         print(n+1);
     }
 }

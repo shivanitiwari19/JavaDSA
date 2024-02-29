8bb30abf7364826d7d7a16c310446f58bb37aee5
shivanitiwari19 <shivani.2125it1065@kiet.edu>
2024-02-29 12:28:10 +0530

    File updated.

--- src/Recursion/Fibonacci.java
+++ src/Recursion/Fibonacci.java
@@ -2,7 +2,8 @@ package Recursion;
 
 public class Fibonacci {
     public static void main(String[] args) {
-        System.out.println(fibo(7));
+        int ans = fibo(4);
+        System.out.println(ans);
     }
 
     static int fibo(int n){
